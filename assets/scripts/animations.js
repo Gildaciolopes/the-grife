8d0da40c7d141042f0cdfb06e24e0f815ab60711
Gildácio Lopes <gildacioj399@gmail.com>
2025-06-08 19:15:31 -0300

    feat: add instagram footer

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".whatsapp-btn, .logo img, h1");
+  const elements = document.querySelectorAll(
+    ".whatsapp-btn, .logo img, h1, .footer-container"
+  );
 
   elements.forEach((el, index) => {
     el.classList.add("animate-in");
